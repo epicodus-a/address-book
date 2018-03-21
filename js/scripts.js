9f509e6a14eaa08ff5ced2d811b3e67d6cdf3944
Adriana C and julia sheremet <adriana@>
2018-03-21 16:28:12 -0700

    make address book functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-  $("form#food-survey").submit(function(event) {
+$().ready(function(){
+  $("form#address-form").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+
+    var anchor = "<a href='#'>" + name + "</a>";
+    $("#firstname").prepend(anchor);
+
+    var detail = "<p>"+name+"</p>";
+    $(".detail").append(detail);
+
+    $("#firstname").click(function(){
+      $(".detail").show();
+    });
+
 
 
   });
